0fb4f151cb75e3740cc0dd2939a06314e3793e59
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2023-11-13 00:02:02 +0100

    update file service with new utility functions

--- services/file.js
+++ services/file.js
@@ -1,28 +1,30 @@
 import fs from "fs";
+import path from "path";
 import csv from "csv-parser";
 import { log } from "./logger.js";
+import { nanoid } from "nanoid";
 
-export const makeDir = (path) => {
-  let error = undefined;
+const JSON_OBJECT_LIMIT_PER_FILE = 300;
 
-  fs.mkdir(path, { recursive: true }, (err) => {
+export const makeDir = (dirPath) => {
+  let error;
+
+  fs.mkdir(dirPath, { recursive: true }, (err) => {
     error = err;
 
     const logMessage = err
-      ? `ERROR, while creating directory at ${path} - error trace: ${err}`
-      : `SUCCESS, while creating directory at ${path}`;
-
-    log(logMessage);
+      ? `ERROR, while creating directory at ${dirPath} - error trace: ${err}`
+      : `SUCCESS, while creating directory at ${dirPath}`;
+    if (err) log(logMessage);
   });
 
-  return error ? undefined : path;
+  return error ? undefined : dirPath;
 };
 
 export const deleteFile = (filePath) => {
   fs.unlink(filePath, (err) => {
-    if (err) {
+    if (err)
       log(`ERROR, while deleting file at ${filePath} - error trace: ${err}`);
-    }
   });
 };
 
@@ -32,7 +34,7 @@ export const writeFileAsync = (filePath, fileContentString) => {
       ? `ERROR, while writing file at ${filePath} - error trace: ${err}`
       : `SUCCESS, while writing file at ${filePath}`;
 
-    log(logMessage);
+    if (err) log(logMessage);
   });
 };
 
@@ -47,7 +49,7 @@ export const appendFileAsync = (filePath, fileContentString) => {
 };
 
 export const csvToArray = (filePath) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const result = [];
     fs.createReadStream(filePath)
       .pipe(csv())
@@ -61,7 +63,66 @@ export const csvToArray = (filePath) => {
         log(
           `ERROR, while parsing csv file at ${filePath} - error trace: ${err}`
         );
-        reject(err);
+        resolve([]);
       });
+  });
+};
+
+export const readDir = (dirPath) => {
+  return new Promise((resolve) => {
+    fs.readdir(dirPath, (err, files) => {
+      const logMessage = err
+        ? `ERROR, while reading files from ${dirPath} - error trace: ${err}`
+        : `SUCCESS, while reading files from ${dirPath}`;
+
+      if (err) {
+        log(logMessage);
+        resolve([]);
+      }
+      resolve(files);
+    });
+  });
+};
+
+export const readJsonFileSync = (filePath) => {
+  try {
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
+    return data;
+  } catch (err) {
+    log(
+      `ERROR, while reading a json file at ${filePath} - error trace: ${err}`
+    );
+  }
+};
+
+export const mergeJsonFiles = async (dirPath) => {
+  let combinedData = [];
+  const fileNames = await readDir(dirPath);
+  fileNames.forEach((fileName, index) => {
+    if (path.extname(fileName) === ".json") {
+      const filePath = path.join(dirPath, fileName);
+      console.log(filePath);
+      const data = readJsonFileSync(filePath);
+
+      if (data) combinedData.push(data);
+
+      if (
+        combinedData.length === JSON_OBJECT_LIMIT_PER_FILE ||
+        index === fileNames.length - 1
+      ) {
+        try {
+          fs.writeFileSync(
+            `commitIDs/dataset-${nanoid()}.json`,
+            JSON.stringify(combinedData, null, 4)
+          );
+        } catch (err) {
+          log(
+            `ERROR, while writing combined JSON file, resulting from ${dirPath} - error trace: ${err}`
+          );
+        }
+        combinedData = [];
+      }
+    }
   });
 };
