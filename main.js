987aee4561cced2a9130f6260e5342685676e5f4
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-01-21 13:20:04 +0100

    env file template

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { mergeJsonFiles } from "./services/file.js";
 import readline from "readline";
 import Secbench from "./repositories/secbench/secbench.js";
 import Ossf from "./repositories/ossf/ossf.js";
+import AbstractSyntaxTree from "abstract-syntax-tree";
+import fs from "fs";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -52,3 +54,35 @@ const main = async () => {
 };
 
 main();
+
+const source = "const answer = 42";
+// const tree = new AbstractSyntaxTree(
+//   fs.readFileSync(
+//     `datasets\\ossf_f - Copy\\vul\\CVE-2016-10735\\twbs\\bootstrap\\2\\ba6a6f13691000ffaf22ef8e731513737659447f\\util.js`,
+//     "utf-8"
+//   )
+// );
+const tree = new AbstractSyntaxTree(`const add = async () => {
+  const a = await 1;
+  const b = 2;
+  const c = a + b;
+  return c;
+}`);
+// printRecursiveObject(tree.find("FunctionDeclaration").length); // [ { type: 'Literal', value: 42 } ]
+// printRecursiveObject(tree.find("FunctionExpression")[0]); // [ { type: 'Literal', value: 42 } ]
+// printRecursiveObject(tree.find("ArrowFunctionExpression")[1]); // [ { type: 'Literal', value: 42 } ]
+printRecursiveObject(tree.find("AwaitExpression")[0]); // [ { type: 'Literal', value: 42 } ]
+
+function printRecursiveObject(obj, indent = 0) {
+  const spaces = " ".repeat(indent * 2);
+
+  for (const key in obj) {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      console.log(`${spaces}${key}: {`);
+      printRecursiveObject(obj[key], indent + 1);
+      console.log(`${spaces}},`);
+    } else {
+      console.log(`${spaces}${key}: "${obj[key]}",`);
+    }
+  }
+}
