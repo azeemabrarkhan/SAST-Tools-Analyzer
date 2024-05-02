19e4d8691caad01f89ba338ece3accc74d86913c
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-05-02 03:56:32 +0200

    ossf file refactoring

--- repositories/ossf/ossf.js
+++ repositories/ossf/ossf.js
@@ -7,13 +7,14 @@ import {
 } from "../../services/file.js";
 import { fetchFile } from "../../services/http.js";
 import { createNewLogFile, log } from "../../services/logger.js";
-import AbstractSyntaxTree from "abstract-syntax-tree";
+import AbstractSynTree from "../../services/abstractSynTree.js";
 
 export default class Ossf {
   vulnerablityCount;
   currentDir;
   metaData;
   metaDataFilePath;
+  abstractSyntaxTree;
 
   constructor() {
     createNewLogFile();
@@ -21,6 +22,7 @@ export default class Ossf {
     this.currentDir = process.cwd();
     this.metaData = [];
     this.metaDataFilePath = `${this.currentDir}\\repositories\\ossf\\metaData.json`;
+    this.abstractSyntaxTree = new AbstractSynTree();
   }
 
   scrape = async () => {
@@ -38,37 +40,14 @@ export default class Ossf {
     this.vulnerablityCount = 0;
   };
 
+  getFileNameWithExt = (location) => {
+    const splitFileName = location.file.split("/");
+    return `${splitFileName[splitFileName.length - 1]}${
+      location.implicitExtension ? `.${location.implicitExtension}` : ""
+    }`;
+  };
+
   createMetaObj = (commit, ownerAndProject, sourceCode) => {
-    const tree = new AbstractSyntaxTree(sourceCode);
-
-    const functionDeclarations = tree
-      .find("FunctionDeclaration")
-      .map((node) => ({
-        type: "FunctionDeclaration",
-        startLine: node?.loc?.start?.line,
-        endLine: node?.loc?.end?.line,
-      }));
-    const functionExpressions = tree.find("FunctionExpression").map((node) => ({
-      type: "FunctionExpression",
-      startLine: node?.loc?.start?.line,
-      endLine: node?.loc?.end?.line,
-    }));
-    const arrowFunctionExpressions = tree
-      .find("ArrowFunctionExpression")
-      .map((node) => ({
-        type: "ArrowFunctionExpression",
-        startLine: node?.loc?.start?.line,
-        endLine: node?.loc?.end?.line,
-      }));
-
-    const functionsInVul = [
-      ...functionDeclarations,
-      ...functionExpressions,
-      ...arrowFunctionExpressions,
-    ]
-      .sort((a, b) => a.startLine - b.startLine)
-      .map((f, index) => ({ name: `function${index}`, ...f }));
-
     const { CVE, CWEs, repository, prePatch, postPatch } = commit;
 
     const metaInfo = {
@@ -81,19 +60,17 @@ export default class Ossf {
       fixPath: "",
       lineNumber: 0,
       explanation: "",
-      functionsInVul,
+      functionsInVul: this.abstractSyntaxTree.getFunctionsLocation(sourceCode),
     };
 
     for (let i = 0; i < prePatch.weaknesses.length; i++) {
-      const splitFileName = prePatch.weaknesses[i].location.file.split("/");
+      const fileNameWithExt = this.getFileNameWithExt(
+        prePatch.weaknesses[i].location
+      );
 
-      metaInfo.vulPath = `/vul/${CVE}/${ownerAndProject}/${
-        this.vulnerablityCount
-      }/${prePatch.commit}/${splitFileName[splitFileName.length - 1]}`;
+      metaInfo.vulPath = `/vul/${CVE}/${ownerAndProject}/${this.vulnerablityCount}/${prePatch.commit}/${fileNameWithExt}`;
 
-      metaInfo.fixPath = `/fix/${CVE}/${ownerAndProject}/${
-        this.vulnerablityCount
-      }/${commit.postPatch.commit}/${splitFileName[splitFileName.length - 1]}`;
+      metaInfo.fixPath = `/fix/${CVE}/${ownerAndProject}/${this.vulnerablityCount}/${commit.postPatch.commit}/${fileNameWithExt}`;
 
       metaInfo.lineNumber = prePatch.weaknesses[i].location.line;
       metaInfo.explanation = prePatch.weaknesses[i].explanation;
@@ -130,14 +107,13 @@ export default class Ossf {
     const vulFileUrl = `${baseUrl}/contents/${fileName}?ref=${prePatch.commit}`;
     const fixFileUrl = `${baseUrl}/contents/${fileName}?ref=${postPatch.commit}`;
 
-    const splitFileName = fileName.split("/");
+    const fileNameWithExt = this.getFileNameWithExt(
+      prePatch.weaknesses[0].location
+    );
 
     return fetchFile(vulFileUrl)
       .then((sourceCode) => {
-        writeFileAsync(
-          `${vulPath}\\${splitFileName[splitFileName.length - 1]}`,
-          sourceCode
-        );
+        writeFileAsync(`${vulPath}\\${fileNameWithExt}`, sourceCode);
         writeFileAsync(
           `${vulPath}\\weaknesses.txt`,
           JSON.stringify(prePatch.weaknesses, null, 2)
@@ -149,17 +125,14 @@ export default class Ossf {
           this.createMetaObj(commit, ownerAndProject, sourceCode);
         } catch (err) {
           log(
-            `ERROR, while splitting source file into functions - ${commit.repository} - error trace: ${err}`
+            `ERROR, while splitting source file into functions ${vulPath}\\${fileNameWithExt} - error trace: ${err}`
           );
         }
       })
       .then(() =>
         fetchFile(fixFileUrl)
           .then((sourceCode) => {
-            writeFileAsync(
-              `${fixPath}\\${splitFileName[splitFileName.length - 1]}`,
-              sourceCode
-            );
+            writeFileAsync(`${fixPath}\\${fileNameWithExt}`, sourceCode);
             writeFileAsync(
               `${fixPath}\\weaknesses.txt`,
               JSON.stringify(prePatch.weaknesses, null, 2)
@@ -169,9 +142,7 @@ export default class Ossf {
             log(
               `ERROR, while fetching post-fix file from the url: ${fixFileUrl} - error trace: ${err}`
             );
-            deleteFile(
-              `${vulPath}\\${splitFileName[splitFileName.length - 1]}`
-            );
+            deleteFile(`${vulPath}\\${fileNameWithExt}`);
           })
       )
       .catch((err) =>
