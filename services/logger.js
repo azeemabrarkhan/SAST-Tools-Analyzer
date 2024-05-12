55544b2d47dfc78e6964e97ec49c62bf04e72206
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-05-12 19:51:06 +0200

    include time stamp in the log filename

--- services/logger.js
+++ services/logger.js
@@ -1,14 +1,13 @@
-import { nanoid } from "nanoid";
 import { deleteFile, appendFileAsync } from "./file.js";
 
 const currentDir = process.cwd();
 let logFilePath;
-
-let lineNumber = 1;
+let lineNumber;
 
 export const createNewLogFile = () => {
+  const date = new Date();
+  logFilePath = `${currentDir}\\log-${date.toDateString()} - HH MM SS ${date.getHours()} ${date.getMinutes()} ${date.getSeconds()}.txt`;
   lineNumber = 1;
-  logFilePath = `${currentDir}\\log-${nanoid()}.txt`;
 };
 
 export const clearLog = () => {
