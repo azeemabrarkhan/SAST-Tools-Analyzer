ddc8772b7d1734c48f75c4501b04d15ea8491f90
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-05-21 21:50:06 +0200

    Improve generativeAI service to output human readable history and get series of responses by passing prompts one after the other

--- services/generativeAI.js
+++ services/generativeAI.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+import { log } from "./logger.js";
 
 dotenv.config();
 
@@ -51,20 +52,50 @@ export default class GenerativeAI {
   }
 
   async askOneTimeQuery(prompt) {
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(prompt.trim());
     const response = await result.response;
     const text = response.text();
     return text;
   }
 
   async chatWithAI(prompt) {
-    const result = await this.chatObject.sendMessage(prompt);
-    const response = await result.response;
-    const text = await response.text();
-    return text;
+    try {
+      const result = await this.chatObject.sendMessage(prompt.trim());
+      const response = await result.response;
+      const text = await response.text();
+      return text;
+    } catch (err) {
+      console.error(err);
+      log(
+        `ERROR, in having a response from generative AI for the prompt: ${prompt
+          .trim()
+          .substring(0, 50)}.... - error trace: ${err}`
+      );
+    }
+  }
+
+  async getSeriesOfResponses(previousData, prompts) {
+    const aiResponses = [];
+    for (const prompt of prompts) {
+      const result = await this.chatWithAI(prompt);
+      aiResponses.push(result);
+    }
+    return { ...previousData, aiResponses };
   }
 
   async getHistory() {
     return await this.chatObject.getHistory();
+  }
+
+  async getReadableHistory() {
+    const history = await this.getHistory();
+    return history
+      .map(
+        (textObj) =>
+          `${textObj.role === "user" ? "User" : "AI-response"}: ${
+            textObj.parts[0].text
+          }\n\n`
+      )
+      .reduce((finalText, currentText) => finalText + currentText, "");
   }
 }
