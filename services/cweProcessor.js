10ab373e78ae11ca87df9e1ef8ac045b80a3f466
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-08-09 17:24:15 +0200

    change cwe percentage to weighted percentage

--- services/cweProcessor.js
+++ services/cweProcessor.js
@@ -27,12 +27,18 @@ export const getDetectedCWEsPercentage = (
   totalCWEsRecord,
   detectedCWEsRecord
 ) => {
+  const totalCWEsCount = Object.values(totalCWEsRecord).reduce(
+    (acc, cweCount) => acc + cweCount,
+    0
+  );
+
   const CWEsPercentage = {};
 
   for (const CWE in totalCWEsRecord) {
-    CWEsPercentage[CWE] = `${
-      (detectedCWEsRecord[CWE] / totalCWEsRecord[CWE] || 0) * 100
-    }%`;
+    CWEsPercentage[CWE] =
+      (detectedCWEsRecord[CWE] / totalCWEsRecord[CWE] || 0) *
+      (totalCWEsRecord[CWE] / totalCWEsCount) *
+      100;
   }
 
   return CWEsPercentage;
