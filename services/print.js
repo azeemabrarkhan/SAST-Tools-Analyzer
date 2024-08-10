c909008fcf190073ae5b282ae2d86174888d45bd
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2024-08-10 17:13:18 +0200

    remove unused code

--- services/print.js
+++ services/print.js
@@ -1,5 +1,3 @@
-import { readJsonFileSync } from "./file.js";
-
 export function printRecursiveObject(obj, indent = 0) {
   const spaces = " ".repeat(indent * 2);
 
@@ -13,12 +11,3 @@ export function printRecursiveObject(obj, indent = 0) {
     }
   }
 }
-
-// print CWEs
-// const meta = readJsonFileSync(
-//   `${process.cwd()}/repositories/ossf/downloadedRecords.json`
-// );
-// console.log(meta.length);
-// console.log(meta.filter((m) => m.functionsInVul.length === 0).length);
-
-// console.log(new Set(meta.map((m) => m.CWEs).reduce((a, b) => [...a, ...b])));
